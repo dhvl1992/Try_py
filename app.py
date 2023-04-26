0cb43c66f17476bf792d87b404cbb677be5c856a
dhvl1992 <43040506+dhvl1992@users.noreply.github.com>
2023-04-26 10:19:13 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import os 
+
+import streamlit as st 
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain 
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper 
+
+apikey = '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'
+
+os.environ['OPENAI_API_KEY'] = apikey
+
+# App framework
+st.title('🦜🔗 YouTube GPT Creator')
+prompt = st.text_input('Plug in your prompt here') 
